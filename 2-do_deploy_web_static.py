a472a441c05c1a8dd749c855c5313f57fb1fb7ed
bakryabdalsalam <bakryabdalsalam6@gmail.com>
2023-07-06 03:43:30 -0700

    fix error in task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,48 +1,48 @@
 #!/usr/bin/python3
+"""
+Fabric script to distribute an archive to web servers using do_deploy function
+"""
+
 from fabric.api import *
-import os
-from datetime import datetime
+import os.path
 
 env.hosts = ['18.209.178.99', '54.157.165.107']
+env.user = 'ubuntu'
+env.key_filename = '/alx-system_engineering-devops/0x0B-ssh/school.pub'
 
 
 def do_deploy(archive_path):
-    a_list = archive_path.split(".tgz")
-    archive_wo_ext = "".join(a_list)
-    b_list = archive_wo_ext.split("versions/")
-    archive_wo_ext_ver = "".join(b_list)
-    c_list = archive_path.split("versions/")
-    archive_wo_ver = "".join(c_list)
-    if archive_path:
-        put(archive_path, '/tmp/')
-        run("mkdir -p /data/web_static/releases/{}/".
-            format(archive_wo_ext_ver))
-        run("tar -zxf /tmp/{} -C /data/web_static/releases/{}/"
-            .format(archive_wo_ver, archive_wo_ext_ver))
-        run("rm -r /tmp/{}".format(archive_wo_ver))
-        run("mv /data/web_static/releases/{}/web_static/*\
-        /data/web_static/releases/{}/".format(archive_wo_ext_ver,
-                                              archive_wo_ext_ver))
-        run("rm -rf /data/web_static/releases/{}/web_static".
-            format(archive_wo_ext_ver))
-        run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-            format(archive_wo_ext_ver))
-        print("New version deployed!")
-        return True
-    else:
+    """
+    Distributes an archive to web servers
+    """
+    if not os.path.exists(archive_path):
         return False
 
-
-def do_pack():
     try:
-        filepath = "versions/web_static_" + datetime.now().\
-                   strftime("%Y%m%d%H%M%S") + ".tgz"
-        local("mkdir -p versions")
-        local("tar -zcvf versions/web_static_$(date +%Y%m%d%H%M%S).tgz\
-        web_static")
-        print("web_static packed: {} -> {}".
-              format(filepath, os.path.getsize(filepath)))
-    except:
-        return None
+        archive_name = os.path.basename(archive_path)
+        archive_no_ext = os.path.splitext(archive_name)[0]
+
+        # Upload the archive to the /tmp/ directory of the web server
+        put(archive_path, "/tmp/")
+
+        # Uncompress the archive to the folder /data/web_static/releases/<archive filename without extension>
+        run("mkdir -p /data/web_static/releases/{}".format(archive_no_ext))
+
+        with cd("/data/web_static/releases/{}".format(archive_no_ext)):
+            run("tar -xzf /tmp/{} -C .".format(archive_name))
+
+        run("rm /tmp/{}".format(archive_name))
+
+        # Delete the symbolic link /data/web_static/current from the web server
+        run("rm -f /data/web_static/current")
+
+        # Create a new symbolic link /data/web_static/current linked to the new version
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(archive_no_ext))
+
+        print("New version deployed!")
+        return True
+
+    except Exception as e:
+        print("Deployment failed:", str(e))
+        return False
 
